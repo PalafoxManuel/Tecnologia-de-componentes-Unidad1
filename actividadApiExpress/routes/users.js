7667093890e713310b627d075f98226d9e4b4027
PalafoxManuel <mapa_21@alu.uabcs.mx>
2024-04-21 16:25:11 -0700

    Peticiones PATCH, PUT & DELETE agregadas y probadas

--- actividadApiExpress/routes/users.js
+++ actividadApiExpress/routes/users.js
@@ -5,24 +5,65 @@ var users = [
   {
     id:1,
     name: 'Jhon',
-    email: '<EMAIL>'
+    email: 'jhon@gmail.com'
   },
   {
     id:2,
     name: 'Jane',
-    email: '<EMAIL>'
+    email: 'jne@hotmail.com'
   }
 ]
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.json(users);
 });
 
 router.post('/', function(req, res, next){
   var user = req.body;
-  user.push(user);
-  res.json(user);
+  user.id = users.length + 1;
+  users.push(user);
+  res.status(201).json(user);
+});
+
+router.put('/:id', function(req, res, next) {
+  var userId = parseInt(req.params.id, 10);
+  var updatedData = req.body;
+
+  var index = users.findIndex(user => user.id === userId);
+
+  if (index !== -1) {
+    users[index] = { ...users[index], ...updatedData }; // Reemplazar el usuario
+    res.json(users[index]); 
+  } else {
+    res.status(404).json({ error: `Usuario con ID ${userId} no encontrado` });
+  }
+});
+
+router.patch('/:id', function(req, res, next) {
+  var userId = parseInt(req.params.id, 10);
+  var updatedFields = req.body;
+
+  var index = users.findIndex(user => user.id === userId);
+
+  if (index !== -1) {
+    users[index] = { ...users[index], ...updatedFields }; 
+    res.json(users[index]); 
+  } else {
+    res.status(404).json({ error: `Usuario con ID ${userId} no encontrado` });
+  }
+});
+
+router.delete('/:id', function(req, res, next) {
+  var userId = parseInt(req.params.id, 10);
+  var index = users.findIndex(user => user.id === userId);
+
+  if (index !== -1) {
+    users.splice(index, 1); 
+    res.json({ message: `Usuario con ID ${userId} ha sido eliminado` });
+  } else {
+    res.status(404).json({ error: `Usuario con ID ${userId} no encontrado` });
+  }
 });
 
 module.exports = router;
